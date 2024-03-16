550db5320f5920bea68b39f48fef1970e00f3238
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-03-16 11:06:57 +0100

    voeg extra gele vierkant toe in sketch.js

--- Mondriaan_tekening/static/sketch.js
+++ Mondriaan_tekening/static/sketch.js
@@ -1,8 +1,15 @@
 function setup() {
     createCanvas(400, 400);
     background(220);
+
     fill(230, 20, 20);
-    rect(100, 0, 300, 300)
+    rect(100, 0, 300, 300);
+
+    fill(40, 20, 200);
+    rect(0, 300, 100, 100);
+    
+    fill(255, 255, 0)
+    rect(350, 350, 100, 50)
 }
   
 function draw() {
