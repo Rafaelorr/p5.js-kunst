a1b36752d29fee0c040d7d9155f8f307b4c72452
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-03-16 11:40:38 +0100

    maak achtergrond van Mondriaan tekening wit

--- Mondriaan_tekening/static/sketch.js
+++ Mondriaan_tekening/static/sketch.js
@@ -1,6 +1,10 @@
 function setup() {
     createCanvas(400, 400);
     background(220);
+    strokeWeight(4);
+
+    fill(255);
+    rect(-20, -20, 500, 500);
 
     fill(230, 20, 20);
     rect(100, 0, 300, 300);
@@ -8,8 +12,8 @@ function setup() {
     fill(40, 20, 200);
     rect(0, 300, 100, 100);
     
-    fill(255, 255, 0)
-    rect(350, 350, 100, 50)
+    fill(255, 255, 0);
+    rect(350, 355, 100, 50);
 }
   
 function draw() {
