da5ae82cff5a08845e458ac3ebb40944983c7dd1
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-04-20 12:08:16 +0200

    voeg http methods toe voor home route in app.py voor server

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["POST","GET"])
 def home():
     if request.method == "POST":
         javascript_file = request.form.get("javascript_file")
