400145e87fe887ca3d22de2b6a7e71699acd2e64
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-06-22 12:32:29 +0200

    voeg pikachu.js toe

---
+++ server/static/javascript_files/pikachu.js
@@ -0,0 +1,28 @@
+function setup() {
+    // maak een canvas van 400 op 400 pixels
+    createCanvas(400, 400);
+    background(220);
+
+    // dikte van lijnen van vormen
+    strokeWeight(3);
+
+    // witte achtergrond
+    fill(255);
+    rect(-20, -20, 500, 500);
+
+    // rode vierkant rechts boven
+
+    // kleur van vorm
+    fill(230, 20, 20);
+    rect(100, 0, 300, 300);
+    
+    // kleur lijnen
+    stroke(0);
+
+    // x punt 1, y punt 1, x punt 2, y punt 2
+    line(0, 75, 98, 75)
+}
+  
+function draw() {
+    
+}
