e6adb700759a22d97a42b3dcc289fdd13aa7a470
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-07-03 20:38:28 +0200

    voeg js file toe gebaseerd op het Commodore 64 programma print

---
+++ server/static/javascript_files/print.js
@@ -0,0 +1,30 @@
+const size = Math.random(1,40);
+let x = 0;
+let y = 0;
+
+function setup() {
+  createCanvas(400, 400);
+    background(0);
+  
+  }
+
+function draw() {
+  fill(230);
+  stroke('white');
+  strokeWeight(2);
+
+  if (random(1) < 0.5) {
+     line(x,y, x+size, y+size);
+  }
+  else {
+     line(x, y+size, x+size, y);
+  }
+  x = x + size;
+  if (x > width ) {
+    x = 0;
+    y = y + size;
+  }
+  if (y > height) {
+    noLoop();
+  }
+}
